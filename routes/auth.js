155b5ed0d3598d2f0217678b294afdcee7e0a43a
myatmin001 <myatmin.dev001@gmail.com>
2021-06-21 23:02:59 +0630

    get logged user api

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,21 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const User = require("../models/User");
 
 //@Route    GET api/auth
 //@Desc     Get logged in user
 //@Access   Private
-router.get('/', (req,res) => {
-    res.send("Get logged in user")
+router.get('/',auth, async (req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 //@Route    POST api/auth
